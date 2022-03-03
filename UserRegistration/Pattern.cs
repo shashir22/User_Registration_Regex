99b58e122b5cf654b40f20fa5493916dfbdaaf38
Shashikumar <shashirgowdas67@gmail.com>
2022-03-03 16:28:23 +0530

    Added:Code to validate the first name

---
+++ UserRegistration/Pattern.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class Patterns
+    {
+        public static string Regex_name = "^[A-Z]{1}[a-z]{2,}$";
+
+
+        public void IsValidFirstName()
+        {
+            bool flag;
+            Console.WriteLine("Enter the first name");
+
+            do
+            {
+                string firstname = Console.ReadLine();
+                flag = Regex.IsMatch(firstname, Regex_name);
+                if (flag == true)
+                    Console.WriteLine("valid");
+                else
+                    Console.WriteLine("Invalid, Type again");
+            }
+            while (flag == false);
+
+        }
+    }
+}
---
+++ UserRegistration/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the user registration validation using c#");
+            Patterns pattern = new Patterns();
+            string[] FirstNames = { "Shashi", "Madhu", "Sohan" };
+            pattern.IsValidFirstName();
+        }
+    }
+}
